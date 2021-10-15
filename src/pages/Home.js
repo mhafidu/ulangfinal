5b2cf293b139354b233da9f4b9afefc44a727cd9
mhafidu <galiwangkah@gmail.com>
2021-10-15 21:08:46 +0700

    push11

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -67,7 +67,7 @@ function Home() {
             <div ref={container} className="container-home">
                 <div ref={container} className="forms-container">
                     <div className="signin-signup">
-                    <form action="#" className="sign-in-form form">
+                    <form className="sign-in-form form">
                         <h2 className="title"><b>Sign in</b></h2>
                         <div className="input-field">
                         <svg className="icon-form" xmlns="http://www.w3.org/2000/svg" id="Outline" viewBox="0 0 24 24" width="512" height="512"><path d="M12,12A6,6,0,1,0,6,6,6.006,6.006,0,0,0,12,12ZM12,2A4,4,0,1,1,8,6,4,4,0,0,1,12,2Z"/><path d="M12,14a9.01,9.01,0,0,0-9,9,1,1,0,0,0,2,0,7,7,0,0,1,14,0,1,1,0,0,0,2,0A9.01,9.01,0,0,0,12,14Z"/></svg>
@@ -78,10 +78,8 @@ function Home() {
                         <input type="password" placeholder="Password" onChange={handlePasswordAfter}/>
                         </div>
                         <input type="submit" value="Login" className="btn1 solid" onClick={handleSubmitAfter}/>
-                        
-                        
                     </form>
-                    <form action="#" className="sign-up-form form">
+                    <form  className="sign-up-form form">
                         <h2 className="title"><b>Sign up</b></h2>
                         <div className="input-field">
                         <svg className="icon-form" xmlns="http://www.w3.org/2000/svg" id="Outline" viewBox="0 0 24 24" width="512" height="512"><path d="M12,12A6,6,0,1,0,6,6,6.006,6.006,0,0,0,12,12ZM12,2A4,4,0,1,1,8,6,4,4,0,0,1,12,2Z"/><path d="M12,14a9.01,9.01,0,0,0-9,9,1,1,0,0,0,2,0,7,7,0,0,1,14,0,1,1,0,0,0,2,0A9.01,9.01,0,0,0,12,14Z"/></svg>
