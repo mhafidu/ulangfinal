d94dcddf5e6eb40e7ecea312010ff4711411691c
mhafidu <galiwangkah@gmail.com>
2021-10-15 21:31:00 +0700

    push13

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -77,7 +77,8 @@ function Home() {
                         <svg className="icon-form" xmlns="http://www.w3.org/2000/svg" id="Outline" viewBox="0 0 24 24" width="512" height="512"><path d="M19,8.424V7A7,7,0,0,0,5,7V8.424A5,5,0,0,0,2,13v6a5.006,5.006,0,0,0,5,5H17a5.006,5.006,0,0,0,5-5V13A5,5,0,0,0,19,8.424ZM7,7A5,5,0,0,1,17,7V8H7ZM20,19a3,3,0,0,1-3,3H7a3,3,0,0,1-3-3V13a3,3,0,0,1,3-3H17a3,3,0,0,1,3,3Z"/><path d="M12,14a1,1,0,0,0-1,1v2a1,1,0,0,0,2,0V15A1,1,0,0,0,12,14Z"/></svg>
                         <input type="password" placeholder="Password" onChange={handlePasswordAfter}/>
                         </div>
-                        <input type="submit" value="Login" className="btn1 solid" onClick={handleSubmitAfter}/>
+                        
+                        <button type="submit"  className="btn1 solid" onClick={handleSubmitAfter}>Login</button>
                     </form>
                     <form onSubmit={handleSubmitAfter} className="sign-up-form form">
                         <h2 className="title"><b>Sign up</b></h2>
@@ -89,7 +90,8 @@ function Home() {
                         <svg className="icon-form" xmlns="http://www.w3.org/2000/svg" id="Outline" viewBox="0 0 24 24" width="512" height="512"><path d="M19,8.424V7A7,7,0,0,0,5,7V8.424A5,5,0,0,0,2,13v6a5.006,5.006,0,0,0,5,5H17a5.006,5.006,0,0,0,5-5V13A5,5,0,0,0,19,8.424ZM7,7A5,5,0,0,1,17,7V8H7ZM20,19a3,3,0,0,1-3,3H7a3,3,0,0,1-3-3V13a3,3,0,0,1,3-3H17a3,3,0,0,1,3,3Z"/><path d="M12,14a1,1,0,0,0-1,1v2a1,1,0,0,0,2,0V15A1,1,0,0,0,12,14Z"/></svg>
                             <input type="password"   placeholder="Password" onChange={handlePassword}/>
                         </div>
-                        <input type="submit" className="btn1 solid" value="Sign up" onClick={handleSubmit}/>
+                        
+                        <button type="submit" className="btn1 solid"  onClick={handleSubmit}>Sign up</button>
                        
                         
                     </form>
